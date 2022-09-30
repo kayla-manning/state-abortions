92daad0ce0ea355b30f986533e4ee3365f703045
Kayla Manning <kaylamanning@college.harvard.edu>
2022-09-30 13:33:54 -0400

    moving over to latex

--- throwaway.R
+++ throwaway.R
@@ -3,28 +3,15 @@ crimson <- '#dc143c'
 # ggplot theme
 
 theme_thesis <- function() {
-  theme_classic(base_size=12, base_family="Times") %+replace%
-    theme(
-      plot.title = element_text(size=20,  family="Times", face = "bold"),
-      plot.subtitle = element_text(size=12,  family="Times", color="#717171", 
-                                   face = "italic",
-                                   margin = margin(t = 0, r = 0, b = 1, l = 0)),
-      plot.caption = element_text(size=8,  family="Times", hjust = 1),
-      axis.text.x = element_text(size=10,  family="Times"),
-      axis.title.x = element_text(size=14, family="Times", 
-                                  margin = margin(t = 10, r = 0, b = 0, l = 0), face = "bold"),
-      axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), 
-                                  size=14, family="Times", angle=90, face ='bold'),
-      legend.title = element_text(size=10, family="Times"),
-      legend.text = element_text(size=10, family="Times"),
-      legend.position = "bottom",
-      axis.ticks = element_blank()
-    )
+  theme_grey(base_family="Times") +
+    theme(panel.background = element_blank(),
+          axis.line = element_line(colour = 'black'))
 }
 
 # testing
 
-within_all + labs(title = 'example title', 
+within_all + labs(title = 'Example title', 
                   x = 'testing x-axis', 
                   y = 'testing y-axis',
-                  subtitle = 'placeholder subtitle') + theme_thesis()
+                  subtitle = 'placeholder subtitle') + theme_classic(base_family="Times")
+
